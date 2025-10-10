f72405c222228d90fab7c28aaff6e0f2ab8bab0c
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2025-10-10 12:36:05 -0700

    Update config for timberhearth

--- servers/timberhearth/default.nix
+++ servers/timberhearth/default.nix
@@ -5,15 +5,5 @@
     hostName = "timberhearth";
     hostId = "41bc3559";
     useNetworkd = true;
-    vlans = {
-      servers = {
-        id = 4;
-        interface = "enp1s0";
-      };
-    };
-    interfaces = {
-      enp1s0.useDHCP = false;
-      servers.useDHCP = true;
-    };
   };
 }
