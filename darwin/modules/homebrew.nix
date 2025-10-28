c2fdb596d3c04ff5d5c3155148fde371faca5e30
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2025-10-28 09:16:34 -0700

    Remove deprecated homebrew cask and add chrome

--- darwin/modules/homebrew.nix
+++ darwin/modules/homebrew.nix
@@ -1,43 +1,43 @@
 {
   homebrew = {
-  	enable = true;
-  	global.autoUpdate = false;
-  	onActivation = {
-  	  cleanup = "zap";
-  	  upgrade = true;
-  	};
-  	caskArgs = {
-  	  appdir = "/Applications/homebrew";
-  	  require_sha = true;
-  	};
-  	brews = [
+    enable = true;
+    global.autoUpdate = false;
+    onActivation = {
+      cleanup = "zap";
+      upgrade = true;
+    };
+    caskArgs = {
+      appdir = "/Applications/homebrew";
+    };
+    brews = [
       "mas"
-  	];
-  	masApps = {
-  	  Xcode = 497799835;
-  	  Infuse = 1136220934;
-  	  "Wifi Explorer" = 494803304;
-  	  "Windows App" = 1295203466;
-  	  "Microsoft Word" = 462054704;
-  	};
-  	casks = [
-  	  # utilities
-  	  "1password"
-  	  "seafile-client"
-  	  "tailscale-app"
-			"scroll-reverser"
+    ];
+    masApps = {
+      Xcode = 497799835;
+      Infuse = 1136220934;
+      "Wifi Explorer" = 494803304;
+      "Windows App" = 1295203466;
+      "Microsoft Word" = 462054704;
+    };
+    casks = [
+      # utilities
+      "1password"
+      "seafile-client"
+      "tailscale-app"
+      "scroll-reverser"
       "stats"
-  	  # development
-  	  "docker-desktop"
-  	  "visual-studio-code"
-  	  "wireshark-app"
-  	  "alacritty"
-  	  # media
-  	  "firefox"
-  	  "zen"
-  	  "discord"
-  	  "prismlauncher"
-  	  "blender"
-  	];
+      # development
+      "docker-desktop"
+      "visual-studio-code"
+      "wireshark-app"
+      # media
+      "google-chrome"
+	  "spotify"
+      "firefox"
+      "zen"
+      "discord"
+      "prismlauncher"
+      "blender"
+    ];
   };
 }
