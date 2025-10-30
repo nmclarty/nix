451b22c094608cc45e217f392e04660a56642dc4
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2025-10-30 11:33:46 -0700

    Cleaned up formatting

--- hosts/nevan-macbook/darwin-configuration.nix
+++ hosts/nevan-macbook/darwin-configuration.nix
@@ -1,26 +1,5 @@
 { flake, ... }:{
   imports = [
-    flake.modules.darwin.homebrew
-    flake.modules.darwin.flakes
+    flake.modules.darwin.default
   ];
-  # version
-  system.stateVersion = 6;
-  # nix settings
-  nix = {
-    # for determinate nix
-  	enable = false;
-  	settings = {
-    	experimental-features = [ "nix-command" "flakes" ];
-   		warn-dirty = false;
-  	};
-  };
-  # set arch
-  nixpkgs.hostPlatform = "aarch64-darwin";
-  # pam
-  security.pam.services.sudo_local.touchIdAuth = true;
-  # users
-  users.users.nmclarty.home = "/Users/nmclarty";
-  system.primaryUser = "nmclarty";
-  # fish
-  programs.fish.enable = true;
 }
---
+++ modules/darwin/default.nix
@@ -0,0 +1,7 @@
+{
+  imports = [
+    ./flakes.nix
+    ./homebrew.nix
+    ./system.nix
+  ];
+}
