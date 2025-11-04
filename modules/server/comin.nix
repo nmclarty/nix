c344c19c846c213e8ebbbc612fb9f0c02e0b62ce
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2025-11-03 20:11:47 -0800

    Added local remote and deployment status

--- modules/server/comin.nix
+++ modules/server/comin.nix
@@ -1,15 +1,24 @@
-{inputs, config, ...}: {
+{ inputs, config, pkgs, ... }: {
   imports = [ inputs.comin.nixosModules.comin ];
   sops.secrets."github/token".sopsFile = "${inputs.nix-private}/secrets.yaml";
   services.comin = {
     enable = true;
     remotes = [
       {
-        name = "origin";
+        name = "github";
         url = "https://github.com/nmclarty/nix.git";
         branches.main.name = "main";
         auth.access_token_path = config.sops.secrets."github/token".path;
       }
+      {
+        name = "local";
+        url = "/home/nmclarty/projects/nix";
+        branches.main.name = "main";
+        poller.period = 2;
+      }
     ];
+    postDeploymentCommand = pkgs.writers.writeBash "post" ''
+      env | grep COMIN_ > /var/lib/comin/deployment.status
+    '';
   };
 }
