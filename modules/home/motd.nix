fbb6e149efbb4ce3395fc1de5ace4eac82f3c386
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2025-11-06 17:06:26 -0800

    Ensure motd module will only run on supported (linux) platforms

--- modules/home/motd.nix
+++ modules/home/motd.nix
@@ -63,41 +63,44 @@ let
   '';
 in
 {
-  home.packages = with pkgs; [
-    unstable.rust-motd
-    figlet
-    lolcat
-  ];
-  xdg = {
-    # ensure the state dir exists, so cg_stats works
-    stateFile."rust-motd/.empty".text = "";
-    configFile."rust-motd/config.kdl".text =
-      let
-        # creates a list of services without dashes in their names (only main, not their dependencies)
-        services = lib.filter (s: ! lib.strings.hasInfix "-" s) (builtins.attrNames osConfig.virtualisation.quadlet.containers);
-        # turns that list into rust-motd container entries
-        containers = lib.concatStringsSep "\n    " (map (s: "container display-name=\"${s}\" docker-name=\"/${s}\"") services);
-      in
-      ''
-        global {
-          version "1.0"
-          progress-empty-character "-"
-        }
-        components {
-          command "hostname | figlet | lolcat -f"
-          filesystems {
-            filesystem name="nix" mount-point="/nix"
-            filesystem name="services" mount-point="/srv"
+  config = lib.mkIf pkgs.platform.isLinux {
+    home.packages = with pkgs; [
+      unstable.rust-motd
+      figlet
+      lolcat
+    ];
+    xdg = {
+      # ensure the state dir exists, so cg_stats works
+      stateFile."rust-motd/.empty".text = "";
+      configFile."rust-motd/config.kdl".text =
+        let
+          # creates a list of services without dashes in their names (only main, not their dependencies)
+          # and turns that list into rust-motd container entries
+          containers = lib.concatStringsSep "\n    "
+            (map (s: "container display-name=\"${s}\" docker-name=\"/${s}\"")
+              (lib.filter (s: ! lib.strings.hasInfix "-" s) (builtins.attrNames osConfig.virtualisation.quadlet.containers)));
+        in
+        ''
+          global {
+            version "1.0"
+            progress-empty-character "-"
           }
-          uptime prefix="Uptime:"
-          load-avg format="Load (1, 5, 15 min.): {one:.02}, {five:.02}, {fifteen:.02}"
-          cg-stats state-file="${config.xdg.stateHome}/rust-motd/cg_stats.toml" threshold=0.01
-          docker title="Podman" {
-            ${containers}
+          components {
+            command "hostname | figlet | lolcat -f"
+            filesystems {
+              filesystem name="nix" mount-point="/nix"
+              filesystem name="services" mount-point="/srv"
+            }
+            uptime prefix="Uptime:"
+            load-avg format="Load (1, 5, 15 min.): {one:.02}, {five:.02}, {fifteen:.02}"
+            cg-stats state-file="${config.xdg.stateHome}/rust-motd/cg_stats.toml" threshold=0.01
+            docker title="Podman" {
+              ${containers}
+            }
+            command color="light-white" "${last-updated}/bin/last-updated nixpkgs unstable"
+            command color="light-white" "${backup-status}/bin/backup-status local remote"
           }
-          command color="light-white" "${last-updated}/bin/last-updated nixpkgs unstable"
-          command color="light-white" "${backup-status}/bin/backup-status local remote"
-        }
-      '';
+        '';
+    };
   };
 }
