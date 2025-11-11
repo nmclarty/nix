f408c3c020f09b34f426a589db37f0e05f333b87
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2025-11-10 23:48:37 -0800

    Add comments and clean up update status

--- scripts/update-status.py
+++ scripts/update-status.py
@@ -1,17 +1,28 @@
-import json
-from datetime import datetime, timezone
+from json import load, loads
+from datetime import datetime, timezone, timedelta
 from subprocess import run
-import sys
+from sys import argv
 
 
-def diff(channel):
-    now = datetime.now()
+def calculate_diff(input: str) -> list[str | timedelta]:
+    """Calculates the time difference between now and the last modified time of
+    a nix flake input (i.e. nixpkgs).
+
+    :param input: The nix flake input name to check
+    :return: A list containing the input name and the time difference
+    """
     then = datetime.fromtimestamp(
-      data["nodes"][channel]["locked"]["lastModified"])
-    return [channel, now - then]
+      flake_lock["nodes"][input]["locked"]["lastModified"])
+    return [input, datetime.now() - then]
 
 
-def parse_status(status: dict) -> dict:
+def parse_status(status: dict[str, dict]) -> dict[str, str]:
+    """Extracts and transforms a dictionary containing the status received from
+    Comin that will be formatted and displayed to the user.
+
+    :param status: The parsed json status from Comin
+    :return: The status fields to be formatted and displayed to the user
+    """
     deployment = status["deployer"]["deployment"]
     generation = deployment["generation"]
     builder = status["builder"]["generation"]
@@ -38,22 +49,25 @@ def parse_status(status: dict) -> dict:
 
 
 # path to the lock file
-flake_path = sys.argv[1] + "/flake.lock"
+flake_lock_path = argv[1] + "/flake.lock"
 # the flake inputs to check
-inputs = sys.argv[2:]
+inputs = argv[2:]
 
-with open(flake_path, "r") as file:
-    data = json.load(file)
+# load the flake lock file
+with open(flake_lock_path, "r") as file:
+    flake_lock = load(file)
 
-last = map(diff, inputs)
-
+# load the status info in json from comin, and the parse it
 status = parse_status(
-    json.loads(run(["comin", "status", "--json"],
-                   capture_output=True, text=True, check=True).stdout))
+    loads(run(["comin", "status", "--json"],
+              capture_output=True, text=True, check=True).stdout))
+
+# don't show extra info if there is none
+extra_info = f' [{status["extra"]}]' if status["extra"] else ""
 
 print("Updates: ")
-print(f'  Status: ({status["status"]}) {status["ago"]} [{status["extra"]}]')
+print(f'  Status: ({status["status"]}) {status["ago"]}{extra_info}')
 print(f'  Commit: ({status["sha"][:7]}) "{status["msg"]}"')
 print("  Inputs:")
-for i in last:
+for i in map(calculate_diff, inputs):
     print(f'    {i[0]}: {str(i[1])[:-7]} ago')
