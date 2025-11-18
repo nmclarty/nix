1f0018118974b7581fa4cd58caa9f8f0f16ef9b9
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2025-11-17 17:47:43 -0800

    Let's see if this works

--- modules/home/motd.nix
+++ modules/home/motd.nix
@@ -6,7 +6,7 @@ let
     (builtins.readFile "${flake}/scripts/backup-status.py");
 in
 {
-  config = lib.mkIf osConfig.virtualisation.quadlet.enable {
+  config = lib.mkIf osConfig.virtualisation.quadlet.enable || false {
     home.packages = with pkgs; [
       rust-motd
       figlet
