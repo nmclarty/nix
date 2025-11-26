a448ee135de542441e28112ddd1f1f68da2f260f
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2025-11-26 09:55:54 -0800

    Switch nixpkgs to 25.11 from unstable

--- flake.nix
+++ flake.nix
@@ -2,21 +2,21 @@
   description = "A NixOS flake for managing my computers.";
   inputs = {
     # system
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # utilities
     nix-darwin.url = "github:nix-darwin/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nixos-wsl.url = "github:nix-community/nixos-wsl";
     nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
-    # utilities
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-private.url = "github:nmclarty/nix-private";
+    nix-private.inputs.nixpkgs.follows = "nixpkgs";
+    # extras
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
-    nix-private.url = "github:nmclarty/nix-private";
-    nix-private.inputs.nixpkgs.follows = "nixpkgs";
-    # extras
     quadlet-nix.url = "github:seiarotg/quadlet-nix";
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
