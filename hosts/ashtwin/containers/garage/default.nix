1f4a7d8f6df356f886f782ca0e99bc37189c0bfb
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2025-11-30 03:08:50 -0800

    Formatted files

--- hosts/ashtwin/containers/garage/default.nix
+++ hosts/ashtwin/containers/garage/default.nix
@@ -1,4 +1,4 @@
-{config, ...}: {
+{ config, ... }: {
   imports = [ ./config.nix ];
   virtualisation.quadlet = {
     containers = {
@@ -11,7 +11,7 @@
           environments = {
             GARAGE_RPC_SECRET_FILE = "/run/secrets/garage_rpc";
           };
-          secrets = ["garage_rpc,uid=2000,gid=2000,mode=0400"];
+          secrets = [ "garage_rpc,uid=2000,gid=2000,mode=0400" ];
           volumes = [
             "${config.sops.templates."garage/garage.toml".path}:/etc/garage.toml:ro"
             "/srv/garage/meta:/var/lib/garage/meta"
