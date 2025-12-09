de4f80f6b6b6c6fac696a2c04786748bc547b4c5
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2025-12-09 00:32:17 -0800

    Tweak git config

--- modules/home/programs/git.nix
+++ modules/home/programs/git.nix
@@ -2,16 +2,39 @@
   programs.git = {
     enable = true;
     settings = {
+      # personal
       user = {
         name = "Nevan McLarty";
         email = "37232202+nmclarty@users.noreply.github.com";
       };
+
+      # signing
+      tag.gpgSign = true;
+      commit.gpgSign = true;
+      gpg = {
+        format = "ssh";
+        ssh.defaultKeyCommand = "ssh-add -L";
+      };
+
+      # ui
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+
+      # general
       init.defaultBranch = "main";
-      gpg.ssh.defaultKeyCommand = "ssh-add -L";
+      push = {
+        autoSetupRemote = true;
+        followTags = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
     };
-    signing = {
-      format = "ssh";
-      signByDefault = true;
-    };
+    ignores = [
+      ".DS_Store"
+    ];
   };
 }
